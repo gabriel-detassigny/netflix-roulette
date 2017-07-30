d208e7db946024f4a1a6444bde24478d633f9be3
Gabriel de Tassigny <gabriel@multivac.cc>
2017-07-30 21:18:52 +0100

    Add Movie class

---
+++ src/Show/Movie.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace GabrielDeTassigny\NetflixRoulette\Show;
+
+class Movie implements Show
+{
+    private const TYPE = 'movie';
+
+    private $id;
+    private $netflixId;
+    private $title;
+    private $releaseYear;
+    private $rating;
+    private $category;
+    private $cast;
+    private $director;
+    private $summary;
+    private $posterUrl;
+    private $netflixUrl;
+    private $runtime;
+
+    /**
+     * @param array $attributes
+     */
+    public function __construct(array $attributes)
+    {
+        $this->id = $attributes['unit'];
+        $this->netflixId = $attributes['show_id'];
+        $this->title = $attributes['show_title'];
+        $this->releaseYear = (int) $attributes['release_year'];
+        $this->rating = (float) $attributes['rating'];
+        $this->category = $attributes['category'];
+        $this->cast = $attributes['show_cast'];
+        $this->director = $attributes['director'];
+        $this->summary = $attributes['summary'];
+        $this->posterUrl = $attributes['poster'];
+        $this->netflixUrl = self::NETFLIX_BASE_URL . $this->getNetflixId();
+        $this->runtime = (int) $attributes['runtime'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNetflixId(): int
+    {
+        return $this->netflixId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReleaseYear(): int
+    {
+        return $this->releaseYear;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRating(): float
+    {
+        return $this->rating;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCast(): string
+    {
+        return $this->cast;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDirector(): string
+    {
+        return $this->director;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSummary(): string
+    {
+        return $this->summary;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPosterUrl(): string
+    {
+        return $this->posterUrl;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNetflixUrl(): string
+    {
+        return $this->netflixUrl;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRuntime(): int
+    {
+        return $this->runtime;
+    }
+}
---
+++ tests/Show/MovieTest.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabriel
+ * Date: 30/07/2017
+ * Time: 21:17
+ */
+
+namespace GabrielDeTassigny\NetflixRoulette\Tests\Show;
+
+use GabrielDeTassigny\NetflixRoulette\Show\Movie;
+
+class MovieTest extends \PHPUnit_Framework_TestCase
+{
+    /** Movie */
+    private $movie;
+
+    public function setUp()
+    {
+        $attributes = [
+            'unit' => 1,
+            'show_id' => 2,
+            'show_title' => 'Test',
+            'release_year' => '1998',
+            'rating' => '3.5',
+            'category' => 'science-fiction',
+            'show_cast' => 'This Guy, That Guy, That Other Guy',
+            'director' => 'This Dude',
+            'summary' => 'Some random movie',
+            'poster' => 'http://example.com/image.jpg',
+            'runtime' => '142 min'
+        ];
+        $this->movie = new Movie($attributes);
+    }
+
+    public function testGetId()
+    {
+        $this->assertSame(1, $this->movie->getId());
+    }
+
+    public function testGetNetflixId()
+    {
+        $this->assertSame(2, $this->movie->getNetflixId());
+    }
+
+    public function testGetTitle()
+    {
+        $this->assertSame('Test', $this->movie->getTitle());
+    }
+
+    public function testGetReleaseYear()
+    {
+        $this->assertSame(1998, $this->movie->getReleaseYear());
+    }
+
+    public function testGetRating()
+    {
+        $this->assertSame(3.5, $this->movie->getRating());
+    }
+
+    public function testGetCategory()
+    {
+        $this->assertSame('science-fiction', $this->movie->getCategory());
+    }
+
+    public function testGetCast()
+    {
+        $this->assertSame('This Guy, That Guy, That Other Guy', $this->movie->getCast());
+    }
+
+    public function testGetDirector()
+    {
+        $this->assertSame('This Dude', $this->movie->getDirector());
+    }
+
+    public function testGetSummary()
+    {
+        $this->assertSame('Some random show', $this->movie->getSummary());
+    }
+
+    public function testGetPosterUrl()
+    {
+        $this->assertSame('http://example.com/image.jpg', $this->movie->getPosterUrl());
+    }
+
+    public function testGetNetflixUrl()
+    {
+        $this->assertSame('http://www.netflix.com/WiPlayer?movieid=2', $this->movie->getNetflixUrl());
+    }
+
+    public function testGetType()
+    {
+        $this->assertSame('tv-show', $this->movie->getType());
+    }
+
+    public function testGetRuntime()
+    {
+        $this->assertSame(42, $this->movie->getRuntime());
+    }
+}
